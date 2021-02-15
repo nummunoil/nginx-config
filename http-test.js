e5f5daf605ca715ce14beaf4f0937dd62a131f82
Oil Sudarat <sudarat@enres.co>
2021-02-15 11:48:59 +0700

    Add docker-compose

--- http-test.js
+++ http-test.js
@@ -1,11 +1,13 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
+const hostname = "0.0.0.0";
 const port = 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
+  res.write("Hello World! Hello Test!! \n");
+  res.write("query string is... \n");
   res.end(req.url);
 });
 
